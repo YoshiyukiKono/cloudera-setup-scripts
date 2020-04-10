6e61388f91e89a67f5b44ada7f9b7777d7374a40
YoshiyukiKono <yoshiyuki.kono@gmail.com>
2020-04-10 06:48:57 +0000

    update files.

--- psql_config.sh
+++ psql_config.sh
@@ -1,3 +1,9 @@
 #!/bin/bash -x
 
-exec >> ./$(basename $0%.*)_`LANG=c date +%Y%m%d_%H%M`.log 2>&1
+LOG_FILE=$(basename ${0%.*})_`LANG=c date +%y%m%d_%H%M`.log
+
+exec > ./$LOG_FILE 2>&1
+
+date
+
+cat non-exist.file
--- psql_install.sh
+++ psql_install.sh
@@ -4,9 +4,12 @@ exec >> ./$(basename ${0%.*})_`LANG=c date +%y%m%d_%H%M`.log 2>&1
 
 yum install -y postgresql-server
 
-yum install -y python-pip
+#yum install -y python-pip
 
-pip install -y psycopg2==2.7.5 --ignore-installed
+yum install -y epel-release
+yum install -y python-pip --enablerepo=epel
+
+pip install psycopg2==2.7.5 --ignore-installed
 
 echo 'LC_ALL="en_US.UTF-8"' >> /etc/locale.conf
 
