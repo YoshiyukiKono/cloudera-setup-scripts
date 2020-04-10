45e698ef1c16e47ea79f5c15e81ca3a1dc126b35
YoshiyukiKono <yoshiyuki.kono@gmail.com>
2020-04-10 15:19:52 +0900

    Update psql_install.sh

--- psql_install.sh
+++ psql_install.sh
@@ -1,3 +1,16 @@
 #!/bin/bash -x
 
-exec >> ./$(basename $0%.*)_`LANG=c date +%Y%m%d_%H%M`.log 2>&1
+exec >> ./$(basename ${0%.*})_`LANG=c date +%y%m%d_%H%M`.log 2>&1
+
+yum install -y postgresql-server
+
+yum install -y python-pip
+
+pip install -y psycopg2==2.7.5 --ignore-installed
+
+echo 'LC_ALL="en_US.UTF-8"' >> /etc/locale.conf
+
+su -l postgres -c "postgresql-setup initdb"
+
+
+
