639988e4605f528ce57d685b4c9cc649b7f385e1
YoshiyukiKono <yoshiyuki.kono@gmail.com>
2020-04-11 13:07:00 +0900

    Update psql_config.sh

--- psql_config.sh
+++ psql_config.sh
@@ -7,14 +7,14 @@ PG_HBA=/var/lib/pgsql/data/pg_hba.conf
 PG_CONF=/var/lib/pgsql/data/postgresql.conf
 
 sed -i.bak \
- -e 's/^\(host all all 127.0.0.1\/32 ident\)/host all all 127.0.0.1\/32 md5\nhost all all 127.0.0.1\/32 ident/g' \
+ -e 's/^\(host all all 127.0.0.1\/32 ident\)/host all all 127.0.0.1\/32 md5\n\1/g' \
  $PG_HBA
 
 sed -i.bak \
- -e 's/^\(shared_buffers = .*\)/#\1\nshared_buffers = 256MB/g' \
- -e 's/^\(#wal_buffers = .*\)/\1\nwal_buffers = 8MB /g' \ 
- -e 's/^\(#checkpoint_segments = .*\)/\1\ncheckpoint_segments = 16/g' \ 
- -e 's/^\(#checkpoint_completion_target = .*$)/\1\ncheckpoint_completion_target = 0.9/g' \
+ -e 's/\(.*shared_buffers = .*\)/#\1\nshared_buffers = 256MB/g' \
+ -e 's/\(.*wal_buffers = .*\)/#\1\nwal_buffers = 8MB /g' \ 
+ -e 's/\(.*checkpoint_segments = .*\)/#\1\ncheckpoint_segments = 16/g' \ 
+ -e 's/\(.*checkpoint_completion_target = .*\)/#\1\ncheckpoint_completion_target = 0.9/g' \
  $PG_CONF
 
 # -e 's/max_connection/max_connection/g' \
